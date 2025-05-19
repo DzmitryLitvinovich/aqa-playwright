f2a0485644bc9156646d24955d8c46d8e1c98049
Dzmitry Litvinovich <dmitrylitvinovich@gmail.com>
2025-05-20 00:20:32 +0300

    PW-HomeWork-21

---
+++ src/ui/tests/SalesPortal/customers/add-new-customer.spec.ts
@@ -0,0 +1,36 @@
+import test, { expect } from '@playwright/test';
+import { SALES_PORTAL_URL, USER_LOGIN, USER_PASSWORD } from 'config/environment';
+import { generateCustomerData } from 'data/customers/generateCustomer.data';
+import { NOTIFICATIONS } from 'data/notifications.data';
+import { AddNewCustomerPage } from 'ui/pages/customers/addNewCustomer.page';
+import { CustomersPage } from 'ui/pages/customers/customers.page';
+import { HomePage } from 'ui/pages/home.page';
+import { SignInPage } from 'ui/pages/signIn.page';
+
+test.describe('[UI] [Sales Portal] [Customers]', () => {
+  test('Should create customer with valid data', async ({ page }) => {
+    const signInPage = new SignInPage(page);
+    const homePage = new HomePage(page);
+    const customersPage = new CustomersPage(page);
+    const addNewCustomerPage = new AddNewCustomerPage(page);
+
+    await page.goto(SALES_PORTAL_URL);
+    await signInPage.fillCredentials(USER_LOGIN, USER_PASSWORD);
+    await signInPage.clickLogin();
+
+    await homePage.waitForOpened();
+    await homePage.clickModuleButton("Customers");
+    await customersPage.waitForOpened();
+    await customersPage.clickAddNewCustomer();
+    await addNewCustomerPage.waitForOpened();
+    const data = generateCustomerData();
+    await addNewCustomerPage.fillInputs(data);
+    await addNewCustomerPage.clickSaveNewCustomer();
+    await customersPage.waitForOpened();
+    await customersPage.waitForNotification(NOTIFICATIONS.CUSTOMER_CREATED);
+    const createdCustomer = await customersPage.getInfoCreatedCustomer(data.email);
+    await expect(createdCustomer.email).toEqual(data.email);
+    await expect(createdCustomer.name).toEqual(data.name);
+    await expect(createdCustomer.country).toEqual(data.country);
+  });
+});
